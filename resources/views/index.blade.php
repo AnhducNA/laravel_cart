f672b8854f66733ca7da5c08f7ec5fc58edf69d4
SharkDucNA <95276179+AnhducNA@users.noreply.github.com>
2022-07-23 08:57:33 +0700

    cart jquery

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -208,7 +208,7 @@
                                         </div>
                                         <ul>
                                             <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                            <li class="quick-view"><a  href="javascript:">+ Add Cart</a></li>
+                                            <li class="quick-view"><a onclick="addCart(<?php echo $product->id; ?>)" href="javascript:">+ Add Cart</a></li>
                                             <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
                                         </ul>
                                     </div>
@@ -355,6 +355,20 @@
     <script src="assets/js/jquery.slicknav.js"></script>
     <script src="assets/js/owl.carousel.min.js"></script>
     <script src="assets/js/main.js"></script>
+
+    <!-- jquery -->
+    <script>
+        function addCart($id) {
+            $.ajax({
+                url: '',
+                type: '',
+
+            }).done(function(response) {
+                console.log(response);
+            });
+
+        }
+    </script>
 </body>
 
 </html>
